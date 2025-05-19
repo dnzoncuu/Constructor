49dcccf1eec06c7f69ff8babe2a39e96b1a5cef6
dnzoncuu <berkoncu17@gmail.com>
2025-05-19 11:15:45 +0300

    Add project files.

---
+++ Constructor/Baby.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Baby
+    {
+        //Field
+        private string _name;
+        private string _lastName;
+        private DateTime _birthDay;
+        //Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string lastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public DateTime BirthDay
+        {
+            get { return _birthDay; }
+            set { _birthDay = value; }
+        }
+        // Null constructor
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaaaaaaa");
+            BirthDay = DateTime.Now;
+        }
+        //Constructor
+        public Baby(string name , string lastname)
+        {
+            _name = name;
+            _lastName = lastname;
+            Console.WriteLine("Ingaaaaaaaaa");
+            BirthDay = DateTime.Now;
+        }
+        
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,7 @@
+﻿using Constructor;
+
+Baby baby1= new Baby("deniz","öncü");//Nesne Tanımlama
+Baby baby = new Baby();//nesne tanımlama
+
+Console.WriteLine($"{baby1.Name} {baby1.lastName}  {baby1.BirthDay}");
+Console.WriteLine($"{baby.BirthDay}");
